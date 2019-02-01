c509ac4893051f9811526a04d0d17ce23209c259
Harshil Agrawal <ghagrawal17@gmail.com>
2019-02-01 21:59:19 +0530

    calc practical

---
+++ arth-calc/arth-calc/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace arth_calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputA = new System.Windows.Forms.TextBox();
+            this.inputB = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.sub = new System.Windows.Forms.Button();
+            this.mul = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputA
+            // 
+            this.inputA.Location = new System.Drawing.Point(62, 96);
+            this.inputA.Name = "inputA";
+            this.inputA.Size = new System.Drawing.Size(106, 20);
+            this.inputA.TabIndex = 0;
+            // 
+            // inputB
+            // 
+            this.inputB.Location = new System.Drawing.Point(191, 96);
+            this.inputB.Name = "inputB";
+            this.inputB.Size = new System.Drawing.Size(106, 20);
+            this.inputB.TabIndex = 1;
+            // 
+            // add
+            // 
+            this.add.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.add.Location = new System.Drawing.Point(62, 164);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(37, 36);
+            this.add.TabIndex = 2;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sub
+            // 
+            this.sub.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.sub.Location = new System.Drawing.Point(131, 164);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(37, 36);
+            this.sub.TabIndex = 3;
+            this.sub.Text = "-";
+            this.sub.UseVisualStyleBackColor = true;
+            this.sub.Click += new System.EventHandler(this.sub_Click);
+            // 
+            // mul
+            // 
+            this.mul.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.mul.Location = new System.Drawing.Point(191, 164);
+            this.mul.Name = "mul";
+            this.mul.Size = new System.Drawing.Size(37, 36);
+            this.mul.TabIndex = 4;
+            this.mul.Text = "x";
+            this.mul.UseVisualStyleBackColor = true;
+            this.mul.Click += new System.EventHandler(this.mul_Click);
+            // 
+            // div
+            // 
+            this.div.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.div.Location = new System.Drawing.Point(260, 164);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(37, 36);
+            this.div.TabIndex = 5;
+            this.div.Text = "/";
+            this.div.UseVisualStyleBackColor = true;
+            this.div.Click += new System.EventHandler(this.div_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 22);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Answer:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 314);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.mul);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.inputB);
+            this.Controls.Add(this.inputA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputA;
+        private System.Windows.Forms.TextBox inputB;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button sub;
+        private System.Windows.Forms.Button mul;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ arth-calc/arth-calc/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arth_calc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void inputA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ans = Convert.ToInt16(inputA.Text) + Convert.ToInt16(inputB.Text);
+            label1.Text = "Answer: " + ans.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sub_Click(object sender, EventArgs e)
+        {
+            int ans = Convert.ToInt16(inputA.Text) - Convert.ToInt16(inputB.Text);
+            label1.Text = "Answer: " + ans.ToString();
+        }
+
+        private void mul_Click(object sender, EventArgs e)
+        {
+            int ans = Convert.ToInt16(inputA.Text) * Convert.ToInt16(inputB.Text);
+            label1.Text = "Answer: " + ans.ToString();
+        }
+
+        private void div_Click(object sender, EventArgs e)
+        {
+            double ans = Convert.ToDouble(inputA.Text) / Convert.ToDouble(inputB.Text);
+            label1.Text = "Answer: " + ans.ToString();
+        }
+    }
+}
